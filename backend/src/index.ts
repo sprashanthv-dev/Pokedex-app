088538e1dd76f6eeb1b7ce7bd0bb7307ca82cbe7
sprashanthv-cu <Prashanthvamanan3@gmail.com>
2024-06-06 00:47:21 -0600

    feat: Added ESLint + Prettier for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,11 +6,10 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app: Express = express();
 
-
 app.get("/", (req: Request, res: Response) => {
-    res.status(200).json({"message": "Welcome to Pokedex backend server :)"})
-})
+  res.status(200).json({ message: "Welcome to Pokedex backend server :)" });
+});
 
 app.listen(PORT, () => {
-    console.log(`Spreading Poke knowledge since 2024 at port: ${PORT}`);
-})
+  console.log(`Spreading Poke knowledge since 2024 at port: ${PORT}`);
+});
