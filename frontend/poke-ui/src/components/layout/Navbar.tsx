5028929623bea90205ac62d346f6c08148a88c90
sprashanthv-cu <Prashanthvamanan3@gmail.com>
2024-06-16 19:06:22 -0700

    Tailwind setup + navbar styles

--- frontend/poke-ui/src/components/layout/Navbar.tsx
+++ frontend/poke-ui/src/components/layout/Navbar.tsx
@@ -1,14 +1,30 @@
 /*TODO: Show Poke Matchup only when user is logged in
  *       Hide Login menu item once user is logged in */
+
+import "./css/navbar.css";
+
 export const Navbar = () => {
   return (
-    <nav>
-      <img src="../../assets/logo.svg" alt="PokéWorld Logo" />
-      <ul>
-        <li>Generations</li>
-        <li>Types</li>
-        <li>Poké Matchup</li>
-        <li>Login</li>
+    <nav className="flex justify-between items-center navbar-bg">
+      <div className="m-4">
+        <img
+          src="../../assets/logo.svg"
+          alt="PokéWorld Logo"
+          className="w-16 lg:w-44 md:w-24"
+        />
+      </div>
+      {/*TODO: Refactor this to map over li's using a loop */}
+      {/*TODO: Hamburger menu on small screen sizes */}
+      {/*TODO: On hover, highlight the current menu item */}
+      <ul className="flex md:text-xl">
+        <li className="mx-8 hover:cursor-pointer hover:underline">
+          Generations
+        </li>
+        <li className="mx-8 hover:cursor-pointer hover:underline">Types</li>
+        <li className="mx-8 hover:cursor-pointer hover:underline">
+          Poké Matchup
+        </li>
+        <li className="mx-8 hover:cursor-pointer hover:underline">Login</li>
       </ul>
     </nav>
   );
